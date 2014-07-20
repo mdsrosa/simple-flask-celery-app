9d8a6e0f10c82ce4c94e8612e4c9e03df313f317
Matheus Rosa <matheusdsrosa@gmail.com>
2014-07-20 17:04:21 -0300

    Added simple application file and reqs file.

---
+++ app.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+__author__ = 'matheusrosa'
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello World'
+
+
+if __name__ == '__main__':
+    app.run()
