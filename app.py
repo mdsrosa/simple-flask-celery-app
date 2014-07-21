c503638f392a119cfa35faff047e770f9795d323
Matheus Rosa <matheusdsrosa@gmail.com>
2014-07-21 15:46:51 -0300

    Added views functions. Added templates and style.

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 __author__ = 'matheusrosa'
 import sqlite3
-from flask import Flask, g
+from flask import Flask, g, request, session, redirect, url_for, flash, abort, \
+    render_template
 
 app = Flask(__name__)
 app.config.from_object('settings')
@@ -43,6 +44,49 @@ def close_db(error):
     """
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
+
+
+@app.route('/')
+def show_entries():
+    db = get_db()
+    cur = db.execute('select title, text from entries order by id asc')
+    entries = cur.fetchall()
+    return render_template('show_entries.html', entries=entries,
+                           title=u'My Entries')
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    db = get_db()
+    db.execute('insert into entries (title, text) values (?, ?)',
+               [request.form['title'], request.form['text']])
+    db.commit()
+    flash('New entry was successfully posted.')
+    return redirect(url_for('show_entries'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in.')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    session.pop('logged_in')
+    flash('You were logged out.')
+    return redirect(url_for('show_entries'))
+
 
 if __name__ == '__main__':
     app.run()
