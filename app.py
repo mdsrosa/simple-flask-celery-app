26c4c0e4673be8a2dab866bc8d5ba4bb5c7d343d
Matheus Rosa <matheusdsrosa@gmail.com>
2014-07-21 10:52:46 -0300

    Added helper functions for database.
    Created the database. Added some folders for handle static files and
    templates.

--- app.py
+++ app.py
@@ -1,14 +1,48 @@
 # -*- coding: utf-8 -*-
 __author__ = 'matheusrosa'
-
-from flask import Flask
+import sqlite3
+from flask import Flask, g
 
 app = Flask(__name__)
+app.config.from_object('settings')
 
-@app.route('/')
-def index():
-    return 'Hello World'
 
+def connect_db():
+    """
+    Connects to the database
+    :return:
+    """
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+
+def get_db():
+    """
+    Open a new database connection if there is none yet for the
+    current application context.
+    :return:
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    """
+    Close the database again at the end of the request.
+    :return:
+    """
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
 
 if __name__ == '__main__':
     app.run()
