b3d313012530a7e20239cf3d7ee571b3ffb2c86c
Matheus Rosa <matheusdsrosa@gmail.com>
2014-07-22 01:25:53 -0300

    Updated path to schema.sql in application configs

--- application/__init__.py
+++ application/__init__.py
@@ -25,7 +25,7 @@ def connect_db():
 def init_db():
     with app.app_context():
         db = get_db()
-        with app.open_resource('schema.sql', mode='r') as f:
+        with app.open_resource('../schema.sql', mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit()
 
